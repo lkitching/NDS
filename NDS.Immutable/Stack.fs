b461dd4ece84d0080d5c5481451e6cf7889329e6
lkitching <lee.kitching@gmail.com>
2013-03-14 13:57:16 +0000

    Create project and add basic immutable stack module

---
+++ NDS.Immutable/Stack.fs
@@ -0,0 +1,24 @@
+﻿namespace NDS.Immutable
+module Stack =
+
+    type Stack<'a> = Empty | S of 'a * Stack<'a>
+
+    let empty<'T> : Stack<'T> = Empty
+    let push e s = S(e, s)
+    let pop = function
+    | Empty -> failwith "Cannot pop empty stack"
+    | S(e, r) -> (e, r)
+
+    let tryPop = function
+    | Empty -> None
+    | S(e, r) -> Some((e, r))
+
+    let peek = function
+    | Empty -> failwith "Cannot peek at empty stack"
+    | S(e, _) -> e
+
+    let tryPeek = function
+    | Empty -> None
+    | S(e, _) -> Some(e)
+
+    let ofList l = List.foldBack (fun e s -> S(e, s)) l Empty
---
+++ NDS.Immutable/Stack.fsi
@@ -0,0 +1,12 @@
+﻿namespace NDS.Immutable
+module Stack =
+
+    type Stack<'T>
+    val empty<'T> : Stack<'T>
+    val push : 'T -> Stack<'T> -> Stack<'T>
+    val pop : Stack<'T> -> ('T * Stack<'T>)
+    val tryPop : Stack<'T> -> ('T * Stack<'T>) option
+    val peek : Stack<'T> -> 'T
+    val tryPeek : Stack<'T> -> 'T option
+
+    val ofList : 'T list -> Stack<'T>
