1895336303c00a9adfeec64651bba0aa0e254be7
lkitching <lee.kitching@gmail.com>
2013-03-15 14:02:35 +0000

    Add basic unbalanced binary search tree.

---
+++ NDS.Immutable/BinarySearchTree.fs
@@ -0,0 +1,19 @@
+﻿namespace NDS.Immutable
+module BinarySearchTree =
+    type BinarySearchTree<'T> = Empty | Node of BinarySearchTree<'T> * 'T * BinarySearchTree<'T>
+
+    let empty<'T> : BinarySearchTree<'T> = Empty
+
+    let rec add (elem: 'T) = function
+    | Empty -> Node(Empty, elem, Empty)
+    | Node(left, x, right) when elem > x -> Node(left, x, add elem right)
+    | Node(left, x, right) when elem < x -> Node(add elem left, x, right)
+    | Node(_, _, _) as n -> n
+
+    let rec contains elem = function
+    | Empty -> false
+    | Node(_, x, _) when elem = x -> true
+    | Node(_, x, right) when elem > x -> contains elem right
+    | Node(left, x, _) -> contains elem left
+    
+
---
+++ NDS.Immutable/BinarySearchTree.fsi
@@ -0,0 +1,8 @@
+﻿namespace NDS.Immutable
+module BinarySearchTree =
+    type BinarySearchTree<'T> when 'T : comparison
+
+    val empty<'T> : BinarySearchTree<'T> when 'T : comparison
+    val add<'T when 'T : comparison> : 'T -> BinarySearchTree<'T> -> BinarySearchTree<'T>
+    val contains<'T when 'T : comparison> : 'T -> BinarySearchTree<'T> -> bool
+
