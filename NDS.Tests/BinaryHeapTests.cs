e078fc7a6105dc32d38f1c006a58b916e0ac827c
lkitching <lee.kitching@gmail.com>
2014-11-20 23:22:37 +0000

    Add mutable array-based binary heap.

---
+++ NDS.Tests/BinaryHeapTests.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+using System.Diagnostics;
+
+namespace NDS 
+{
+    [TestFixture]
+    public class BinaryHeapTests
+    {
+        [Test]
+        public void ShouldBeEmptyInitially()
+        {
+            var sut = Create<int>();
+            Assert.AreEqual(0, sut.Count);
+        }
+
+        [Test]
+        public void GetMinShouldThrowForEmptyHeap()
+        {
+            var sut = Create<int>();
+            Assert.Throws<InvalidOperationException>(() => { int min = sut.Min; });
+        }
+
+        [Test]
+        public void ShouldIncrementCountAfterInsert()
+        {
+            var sut = Create<int>();
+            sut.Insert(1);
+
+            Assert.AreEqual(1, sut.Count, "Failed to increment count on insert");
+        }
+
+        [Test]
+        public void ShouldGetMin()
+        {
+            var sut = Create<int>();
+            sut.Insert(4);
+            sut.Insert(2);
+            sut.Insert(1);
+
+            AssertMin(1, sut);
+        }
+
+        [Test]
+        public void ShouldRemoveMin()
+        {
+            var sut = Create<int>();
+            sut.Insert(4);
+            sut.Insert(2);
+            sut.Insert(1);
+
+            int min = sut.RemoveMin();
+            AssertMin(1, min);
+        }
+
+        [Test]
+        public void RemoveMinShouldDecrementCount()
+        {
+            var sut = Create<int>();
+            sut.Insert(4);
+            sut.Insert(2);
+            sut.Insert(1);
+
+            int min = sut.RemoveMin();
+            Assert.AreEqual(2, sut.Count, "Failed to decrement count on minimum");
+        }
+
+        [Test]
+        public void ShouldUseCustomComparer()
+        {
+            var comparer = new CustomComparer();
+            var sut = new BinaryHeap<Custom>(comparer);
+
+            Random r = new Random();
+            var items = Enumerable.Repeat(1, 10000).Select(_ => new Custom(r.Next())).ToArray();
+
+            int minValue = items.Select(c => c.Value).Min();
+
+            foreach (Custom item in items)
+            {
+                sut.Insert(item);
+            }
+
+            AssertMin(minValue, sut.Min.Value);
+        }
+
+        [Test]
+        public void ShouldAllowNull()
+        {
+            var sut = new BinaryHeap<Custom>(new CustomComparer());
+            sut.Insert(new Custom(3));
+            sut.Insert(null);
+
+            Assert.IsNull(sut.Min);
+        }
+
+        [Test]
+        public void ShouldInsertItems()
+        {
+            Random r = new Random();
+            var items = Enumerable.Repeat(1, 10000).Select(_ => r.Next()).ToArray();
+
+            var sut = Create<int>();
+
+            foreach (int i in items)
+            {
+                sut.Insert(i);
+            }
+
+            int minItem = items.Min();
+            AssertMin(minItem, sut);
+        }
+
+        [Test]
+        public void ShouldAllowDuplicates()
+        {
+            var sut = Create<int>();
+            sut.Insert(4);
+            sut.Insert(4);
+
+            Assert.AreEqual(2, sut.Count, "Unexpected count");
+            AssertMin(4, sut);
+        }
+
+        private static BinaryHeap<T> Create<T>()
+        {
+            return new BinaryHeap<T>();
+        }
+
+        private class Custom
+        {
+            public Custom(int i)
+            {
+                this.Value = i;
+            }
+
+            public int Value { get; private set; }
+        }
+
+        private class CustomComparer : IComparer<Custom>
+        {
+            public int Compare(Custom x, Custom y)
+            {
+                if (x == null) return y == null ? 0 : -1;
+                if (y == null) return 1;
+
+                Debug.Assert(x != null);
+                Debug.Assert(y != null);
+
+                return x.Value.CompareTo(y.Value);
+            }
+        }
+
+        private static void AssertMin<T>(T expected, T actual)
+        {
+            Assert.AreEqual(expected, actual, "Unexpected minimum value");
+        }
+
+        private static void AssertMin<T>(T expected, BinaryHeap<T> heap)
+        {
+            AssertMin(expected, heap.Min);
+        }
+    }
+}
---
+++ NDS/BinaryHeap.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace NDS 
+{
+    public class BinaryHeap<T>
+    {
+        private readonly IComparer<T> comparer;
+        private T[] items;
+        private int maxDepth;
+        private int count = 0;
+
+        public BinaryHeap()
+            : this(Comparer<T>.Default)
+        {
+        }
+
+        public BinaryHeap(IComparer<T> comparer)
+        {
+            this.comparer = comparer ?? Comparer<T>.Default;
+
+            this.maxDepth = 4;
+            int initialCapacity = GetCapacityForDepth(this.maxDepth);
+            this.items = new T[initialCapacity];
+        }
+
+        public void Insert(T value)
+        {
+            //ensure space for new item
+            this.EnsureCapacityForInsert();
+
+            //insert item at the 'end' of the tree (i.e. right-most item at the deepest level)
+            this.items[this.count] = value;
+
+            //fix heap property
+            this.FixUp(this.count);
+
+            this.count++;
+        }
+
+        public T RemoveMin()
+        {
+            this.GuardNotEmpty();
+
+            //get min
+            T min = this.items[0];
+
+            //move the 'last' item in the tree into the root position
+            this.items[0] = this.items[this.count - 1];
+            this.items[this.count - 1] = default(T);        //remove extra reference to item
+            
+            //fix heap
+            this.FixDown(0);
+
+            this.count--;
+            return min;
+        }
+
+        public T Min
+        {
+            get
+            {
+                this.GuardNotEmpty();
+
+                Debug.Assert(this.items.Length > 0);
+                return this.items[0];
+            }
+        }
+
+        public int Count
+        {
+            get { return this.count; }
+        }
+
+        private void FixUp(int nodeIndex)
+        {
+            while (nodeIndex > 0)
+            {
+                int parentIndex = GetParentIndex(nodeIndex);
+                
+                //swap current node and parent if current < parent
+                if (this.comparer.Compare(this.items[nodeIndex], this.items[parentIndex]) < 0)
+                {
+                    this.SwapNodes(nodeIndex, parentIndex);
+                    nodeIndex = parentIndex;
+                }
+                else break;
+            }
+        }
+
+        private void FixDown(int nodeIndex)
+        {
+            int? minChildIndex = this.GetMinChildIndex(nodeIndex);
+
+            //swap node with min child until it is no smaller
+            while (minChildIndex.HasValue && this.comparer.Compare(this.items[nodeIndex], this.items[minChildIndex.Value]) > 0)
+            {
+                this.SwapNodes(nodeIndex, minChildIndex.Value);
+                minChildIndex = this.GetMinChildIndex(minChildIndex.Value);
+            }
+        }
+
+        private void EnsureCapacityForInsert()
+        {
+            if (this.count < this.items.Length - 1) return;
+
+            int newCapacity = GetCapacityForDepth(this.maxDepth + 1);
+            Debug.Assert(newCapacity > this.items.Length);
+
+            T[] newItems = new T[newCapacity];
+            Array.Copy(this.items, newItems, this.items.Length);
+            this.items = newItems;
+
+            this.maxDepth++;
+        }
+
+        private void GuardNotEmpty()
+        {
+            if (this.count < 1) throw new InvalidOperationException("Heap is empty");
+        }
+
+        private int? GetMinChildIndex(int parentIndex)
+        {
+            Debug.Assert(parentIndex >= 0);
+
+            //left child is at 2n, right at 2n+1
+            int leftChildIndex = parentIndex * 2;
+            int rightChildIndex = leftChildIndex + 1;
+
+            if (leftChildIndex >= this.count) return null;                      //no children
+            else if (rightChildIndex >= this.count) return leftChildIndex;      //left child only
+            else
+            {
+                //both children exist so find smallest
+                //NOTE: right child is chosen if both are equal
+                return this.comparer.Compare(this.items[leftChildIndex], this.items[rightChildIndex]) < 0
+                    ? leftChildIndex
+                    : rightChildIndex;
+            }
+        }
+
+        private void SwapNodes(int i, int j)
+        {
+            T temp = this.items[i];
+            this.items[i] = this.items[j];
+            this.items[j] = temp;
+        }
+
+        private static int GetCapacityForDepth(int depth)
+        {
+            Debug.Assert(depth > 0);
+
+            if (depth > 31) throw new ArgumentOutOfRangeException("Cannot create tree with depth > 31");
+            return (1 << depth) - 1;
+        }
+
+        private static int GetParentIndex(int childIndex)
+        {
+            if(childIndex == 0) return -1;
+            return ((childIndex + 1) / 2) - 1;
+        }
+    }
+}
