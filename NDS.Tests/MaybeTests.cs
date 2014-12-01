a0e5421cc405845f94522f16882190e5f7b26f25
lkitching <lee.kitching@gmail.com>
2014-12-01 21:26:20 +0000

    Add Maybe type.
    Add a Maybe type to uniformly represent explicit optional values.

---
+++ NDS.Tests/MaybeTests.cs
@@ -0,0 +1,415 @@
+﻿using System;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace NDS 
+{
+    [TestFixture]
+    public class MaybeTests
+    {
+        [Test]
+        public void Constructor_Should_Throw_If_Argument_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => { var _ = new Maybe<string>(null); }, "Constructor should throw for null argument");
+        }
+
+        [Test]
+        public void Should_Get_Value()
+        {
+            int i = 4;
+            var m = new Maybe<int>(i);
+
+            Assert.IsTrue(m.HasValue, "Maybe should have value");
+            Assert.AreEqual(i, m.Value, "Maybe value should equal argument");
+        }
+
+        [Test]
+        public void Default_Maybe_Should_Not_Have_Value()
+        {
+            var m = new Maybe<string>();
+            Assert.IsFalse(m.HasValue, "Empty maybe should not have a value");
+        }
+
+        [Test]
+        public void Get_Should_Throw_For_Empty_Maybe()
+        {
+            var m = new Maybe<string>();
+            Assert.Throws<InvalidOperationException>(() => { var _ = m.Value; }, "Getter should throw for empty maybe");
+        }
+
+        [Test]
+        public void GetOr_Should_Return_Value()
+        {
+            int value = 3;
+            var m = new Maybe<int>(value);
+
+            Assert.AreEqual(value, m.GetOr(-1), "GetOr should return inner value");
+        }
+
+        [Test]
+        public void GetOr_Should_Return_Default_For_Empty()
+        {
+            var m = new Maybe<int>();
+            int @default = 1;
+
+            Assert.AreEqual(@default, m.GetOr(@default), "GetOr should return given default for empty");
+        }
+
+        [Test]
+        public void GetOrFunc_Should_Return_Value()
+        {
+            string value = "abc";
+            var m = new Maybe<string>(value);
+
+            Assert.AreEqual(value, m.GetOr(() => "default"), "GetOr(Func) should return inner value");
+        }
+
+        [Test]
+        public void GetOrFunc_Should_Return_Default_For_Empty()
+        {
+            string @default = "default";
+            var m = new Maybe<string>();
+
+            Assert.AreEqual(@default, m.GetOr(() => @default), "GetOr(Func) should return value from func for empty");
+        }
+
+        [Test]
+        public void Should_Map_Value()
+        {
+            string value = "value";
+            var m = new Maybe<string>(value);
+
+            var mapped = m.Select(str => str.ToUpper());
+            Assert.AreEqual(value.ToUpper(), mapped.Value, "Select should map inner value");
+        }
+
+        [Test]
+        public void Should_Map_Empty()
+        {
+            var m = new Maybe<int>();
+            var mapped = m.Select(i => i * 2);
+            Assert.IsFalse(mapped.HasValue, "Transform of empty maybe should be empty");
+        }
+
+        [Test]
+        public void Where_Should_Keep_Value_If_It_Passes_Filter()
+        {
+            int value = 3;
+            var m = new Maybe<int>(value);
+
+            var result = m.Where(i => i < 5);
+            Assert.AreEqual(value, result.Value, "Where should keep value if it passes filter");
+        }
+
+        [Test]
+        public void Where_Should_Filter_Value()
+        {
+            var m = new Maybe<string>("value");
+            var result = m.Where(str => str.Length > 10);
+
+            Assert.IsFalse(result.HasValue, "Where should return empty if value fails filter");
+        }
+
+        [Test]
+        public void Where_Should_Not_Filter_Empty()
+        {
+            bool exec = false;
+            var m = new Maybe<int>();
+
+            var result = m.Where(_ => { exec = true; return true; });
+            Assert.IsFalse(exec, "Where should not run filter for empty maybe");
+            Assert.IsFalse(result.HasValue, "Filter of empty should not have value");
+        }
+
+        [Test]
+        public void SelectMany_Should_Return_Empty()
+        {
+            bool exec = false;
+            var m = new Maybe<int>();
+            var result = m.SelectMany(_ => { exec = true; return new Maybe<string>("value"); });
+
+            Assert.IsFalse(exec, "SelectMany should not invoke bind func for empty");
+            Assert.IsFalse(result.HasValue, "SelectMany should return empty if source is empty");
+        }
+
+        [Test]
+        public void SelectMany_Should_Apply_Bind_Func()
+        {
+            var m = new Maybe<int>(3);
+            var result = m.SelectMany(i => new Maybe<string>(new string('a', i)));
+            Assert.AreEqual("aaa", result.Value, "SelectMany should apply bind func for value");
+        }
+
+        [Test]
+        public void Should_Cast_Empty()
+        {
+            var m = new Maybe<object>();
+            var cast = m.Cast<string>();
+
+            Assert.IsFalse(cast.HasValue, "Cast of empty should be empty");
+        }
+
+        [Test]
+        public void Should_Cast_Value()
+        {
+            int value = 4;
+            var m = new Maybe<object>(value);
+            var cast = m.Cast<IComparable<int>>();
+
+            Assert.AreEqual(value, cast.Value, "Cast value should equal original");
+        }
+
+        [Test]
+        public void OfType_Should_Propagate_Empty()
+        {
+            var m = new Maybe<int>();
+            var conv = m.OfType<object>();
+
+            Assert.IsFalse(conv.HasValue, "OfType of empty should be empty");
+        }
+
+        [Test]
+        public void OfType_Should_Return_Empty_If_Conversion_Fails()
+        {
+            var m = new Maybe<string>("value");
+            var conv = m.OfType<int>();
+
+            Assert.IsFalse(conv.HasValue, "OfType result should be empty if conversion fails");
+        }
+
+        [Test]
+        public void OfType_Should_Cast_Value_If_Conversion_Succeeds()
+        {
+            string value = "value";
+            var m = new Maybe<object>(value);
+            var conv = m.OfType<IEnumerable<char>>();
+
+            Assert.AreEqual(value, conv.Value);
+        }
+
+        [Test]
+        public void Sequence_Should_Be_Empty_For_Empty()
+        {
+            var m = new Maybe<int>();
+            CollectionAssert.IsEmpty(m, "Sequence should be empty for empty maybe");
+        }
+
+        [Test]
+        public void Sequence_Should_Contain_Value()
+        {
+            int value = 3;
+            var m = new Maybe<int>(value);
+
+            CollectionAssert.AreEqual(new[] { value }, m, "Sequence should contain inner value");
+        }
+
+        [Test]
+        public void Should_Not_Equal_Null()
+        {
+            var m = new Maybe<int>();
+            Assert.IsFalse(m.Equals(null), "Maybe should not equal null");
+        }
+
+        [Test]
+        public void Should_Not_Equal_Non_Maybe()
+        {
+            string value = "value";
+            var m = new Maybe<string>(value);
+            Assert.IsFalse(m.Equals(value), "Maybe should not equal non-maybe value");
+        }
+
+        [Test]
+        public void Empty_Values_Should_Be_Equal()
+        {
+            var m1 = new Maybe<int>();
+            var m2 = new Maybe<int>();
+
+            string msg = "Empty maybes should be equal";
+            Assert.IsTrue(m1.Equals(m2), msg);
+            Assert.IsTrue(m2.Equals(m1), msg);
+        }
+
+        [Test]
+        public void Empty_Should_Not_Equal_Non_Empty()
+        {
+            var m1 = new Maybe<int>();
+            var m2 = new Maybe<int>(4);
+
+            var msg = "Empty maybe should not equal non-empty";
+            Assert.IsFalse(m1.Equals(m2), msg);
+            Assert.IsFalse(m2.Equals(m1), msg);
+        }
+
+        [Test]
+        public void Values_Should_Be_Equal_If_Inner_Values_Equal()
+        {
+            string value = "value";
+            var m1 = new Maybe<string>(value);
+            var m2 = new Maybe<string>(value);
+
+            string msg = "Non-empty maybes should be equal if their values are equal";
+            Assert.IsTrue(m1.Equals(m2), msg);
+            Assert.IsTrue(m2.Equals(m1), msg);
+        }
+
+        [Test]
+        public void Values_Should_Not_Be_Equal_If_Inner_Values_Not_Equal()
+        {
+            var m1 = new Maybe<int>(1);
+            var m2 = new Maybe<int>(2);
+
+            string msg = "Non-empty maybe should not be equal if their value are not equal";
+            Assert.IsFalse(m1.Equals(m2), msg);
+            Assert.IsFalse(m2.Equals(m1), msg);
+        }
+
+        [Test]
+        public void Empty_Maybes_Should_Have_Same_HashCodes()
+        {
+            Assert.AreEqual(new Maybe<int>().GetHashCode(), new Maybe<int>().GetHashCode(), "Empty maybes should have same hash code");
+        }
+
+        [Test]
+        public void NonEmpty_Maybes_With_Same_Value_Should_Have_Same_HashCodes()
+        {
+            int value = 7;
+            var m1 = new Maybe<int>(value);
+            var m2 = new Maybe<int>(value);
+
+            Assert.AreEqual(m1.GetHashCode(), m2.GetHashCode(), "Equal maybes should have same hash code");
+        }
+
+        [Test]
+        public void None_Should_Override_ToString()
+        {
+            var m = Maybe.None<string>();
+            Assert.AreEqual("None", m.ToString());
+        }
+
+        [Test]
+        public void Some_Should_Override_ToString()
+        {
+            string value = "test";
+            string expected = string.Format("Some({0})", value);
+
+            var m = Maybe.Some(value);
+            Assert.AreEqual(expected, m.ToString());
+        }
+
+        //extension methods
+
+        [Test]
+        public void Ap_Should_Invoke_Delegate()
+        {
+            string value = "value";
+            var mf = new Maybe<Func<string, int>>(str => str.Length);
+            var m = new Maybe<string>(value);
+
+            var result = mf.Ap(m);
+            Assert.AreEqual(value.Length, result.Value, "Ap should apply delegate to value if both exist");
+        }
+
+        [Test]
+        public void Ap_Should_Return_Empty_If_Delegate_Empty()
+        {
+            int value = 3;
+            var mf = new Maybe<Func<int, int>>();
+            var m = new Maybe<int>(value);
+
+            var result = mf.Ap(m);
+            Assert.IsFalse(result.HasValue, "Ap should return empty if delegate maybe empty");
+        }
+
+        [Test]
+        public void Ap_Should_Return_Empty_If_Value_Empty()
+        {
+            var mf = new Maybe<Func<int, int>>(i => i * 2);
+            var result = mf.Ap(new Maybe<int>());
+            Assert.IsFalse(result.HasValue, "Ap should return empty if value delegate empty");
+        }
+
+        [Test]
+        public void Join_Should_Return_Empty_If_Outer_Empty()
+        {
+            var outer = new Maybe<Maybe<string>>();
+            var result = outer.Join();
+
+            Assert.IsFalse(result.HasValue, "Join should return empty if outer empty");
+        }
+
+        [Test]
+        public void Join_Should_Return_Inner()
+        {
+            var inner = new Maybe<int>(3);
+            var outer = new Maybe<Maybe<int>>(inner);
+
+            var result = outer.Join();
+            Assert.AreEqual(inner, result);
+        }
+
+        [Test]
+        public void ToMaybe_Should_Return_Empty_For_Null_Reference_Type()
+        {
+            string value = null;
+            var m = value.ToMaybe();
+            Assert.IsFalse(m.HasValue, "ToMaybe should return empty for null reference");
+        }
+
+        [Test]
+        public void ToMaybe_Should_Wrap_Non_Null_ReferenceType()
+        {
+            string value = "value";
+            var m = value.ToMaybe();
+            Assert.AreEqual(value, m.Value, "ToMaybe should return value for non-null reference type");
+        }
+
+        [Test]
+        public void ToMaybe_Should_Return_Empty_For_Null_Nullable_Struct()
+        {
+            int? nullable = null;
+            var m = nullable.ToMaybe();
+            Assert.IsFalse(m.HasValue, "ToMaybe should return empty for null nullable struct");
+        }
+
+        [Test]
+        public void ToMaybe_Should_Wrap_Non_Null_Nullable_Struct()
+        {
+            int value = 4;
+            int? nullable = value;
+
+            var m = nullable.ToMaybe();
+            Assert.AreEqual(value, m.Value, "ToMaybe should wrap nullable struct value");
+        }
+
+        [Test]
+        public void ToNullable_Should_Return_Null_For_None()
+        {
+            var m = new Maybe<int>();
+            Assert.IsNull(m.ToNullable(), "ToNullable should return null for empty");
+        }
+
+        [Test]
+        public void ToNullable_Should_Return_Value()
+        {
+            int value = 4;
+            var m = new Maybe<int>(value);
+            Assert.AreEqual(value, m.ToNullable(), "ToNullable should return inner value");
+        }
+
+        [Test]
+        public void ToClass_Should_Return_Null_For_None()
+        {
+            var m = new Maybe<string>();
+            Assert.IsNull(m.ToClass(), "ToClass should return null for empty");
+        }
+
+        [Test]
+        public void ToClass_Should_Return_Value()
+        {
+            string value = ":D";
+            var m = new Maybe<string>(value);
+            Assert.AreEqual(value, m.ToClass(), "ToClass should return inner reference");
+        }
+    }
+}
---
+++ NDS/Maybe.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace NDS 
+{
+    /// <summary>Represents an optional value of some type <typeparamref name="T"/> The default constructor is used to create empty instances with no value.</summary>
+    /// <typeparam name="T">The type of the encapsulated value.</typeparam>
+    public struct Maybe<T> : IEquatable<Maybe<T>>, IEnumerable<T>
+    {
+        private readonly bool hasValue;
+        private readonly T value;
+
+        /// <summary>Creates a new instance with a value.</summary>
+        /// <param name="value">The encapsulated value.</param>
+        /// <exception cref="ArgumentNullException">If the inner value is null.</exception>
+        public Maybe(T value)
+        {
+            if (value == null) throw new ArgumentNullException("value");
+            Contract.EndContractBlock();
+
+            this.hasValue = true;
+            this.value = value;
+        }
+
+        /// <summary>Gets the value from this instance if it exists, otherwise returns the given default value.</summary>
+        /// <param name="default">The default value to return if this instance has no value.</param>
+        /// <returns>The value from this mabye if it exists, otherwise <paramref name="@default"/>.</returns>
+        public T GetOr(T @default = default(T)) { return this.hasValue ? this.value : @default; }
+
+        /// <summary>Gets the value from this instance it it exists, otherwise returns the value returned from the given delegate.</summary>
+        /// <param name="defaultFunc">Delegate to create the default value to return if this instance has none.</param>
+        /// <returns>The value for this instance if it exists otherwise the value returned from <paramref name="defaultFunc"/>.</returns>
+        public T GetOr(Func<T> defaultFunc)
+        {
+            Contract.Requires(defaultFunc != null);
+            return this.hasValue ? this.value : defaultFunc();
+        }
+
+        /// <summaryGets whether this instance has a value.</summary>
+        public bool HasValue { get { return this.hasValue; } }
+
+        /// <summary>Gets the value from this instance if it exists.</summary>
+        /// <exception cref="InvalidOperationException">If this instance has no value.</exception>
+        public T Value
+        {
+            get
+            {
+                if (this.hasValue) return this.value;
+                else throw new InvalidOperationException("Cannot get value for empty Maybe");
+            }
+        }
+
+        /// <summary>Maps the value inside this instance with the given transform.</summary>
+        /// <typeparam name="U">The type returned from <paramref name="mapFunc"/>.</typeparam>
+        /// <param name="mapFunc">Delegate to transform the value for this instance if it exists.</param>
+        /// <returns></returns>
+        public Maybe<U> Select<U>(Func<T, U> mapFunc)
+        {
+            Contract.Requires(mapFunc != null);
+            return this.hasValue ? new Maybe<U>(mapFunc(this.value)) : new Maybe<U>();
+        }
+
+        /// <summary>Filters the value in this instance with the given filter delegate.</summary>
+        /// <param name="predicate">Delegate to filter the value encapsulated by this instance.</param>
+        /// <returns>An empty maybe instance if this instance is empty or if the inner value fails the predicate. Otherwise an instance encapsulating the same inner value.</returns>
+        public Maybe<T> Where(Func<T, bool> predicate)
+        {
+            Contract.Requires(predicate != null);
+            return (!this.hasValue || predicate(this.value)) ? this : new Maybe<T>();
+        }
+
+        /// <summary>Monadic bind operation. Applies the given delegate to the value of this instance if it exists.</summary>
+        /// <typeparam name="U">The type of value encapsulated in the maybe returned from <paramref name="bindFunc"/>.</typeparam>
+        /// <param name="bindFunc">Delegate for the bind operation.</param>
+        /// <returns>The value returned from <paramref name="bindFunc"/> if this instance has a value. Otherwise another empty instance is returned.</returns>
+        public Maybe<U> SelectMany<U>(Func<T, Maybe<U>> bindFunc)
+        {
+            Contract.Requires(bindFunc != null);
+            return this.hasValue ? bindFunc(this.value) : new Maybe<U>();
+        }
+
+        /// <summary>Extended monadic bind operator required for supporting the query pattern.</summary>
+        /// <typeparam name="U">Type for the intermediate maybe value returned by <paramref name="bindFunc"/>.</typeparam>
+        /// <typeparam name="R">Type for the value returned from the transform delegate <paramref name="selector"/>.</typeparam>
+        /// <param name="bindFunc">Delegate for the bind operation.</param>
+        /// <param name="selector">Delegate for the subsequent map transform.</param>
+        /// <returns></returns>
+        public Maybe<R> SelectMany<U, R>(Func<T, Maybe<U>> bindFunc, Func<T, U, R> selector)
+        {
+            Contract.Requires(bindFunc != null);
+            Contract.Requires(selector != null);
+
+            if (this.hasValue)
+            {
+                T val = this.value;
+                return bindFunc(this.value).Select(u => selector(val, u));
+            }
+            else return new Maybe<R>();
+        }
+
+        /// <summary>Casts the inner value for this instance if it exists.</summary>
+        /// <typeparam name="U">The type to cast the inner value to.</typeparam>
+        /// <returns>A maybe encapsulating a value of the converted type.</returns>
+        /// <exception cref="ClassCastException">If the inner value cannot be cast to type <typeparamref name="U"/>.</exception>
+        public Maybe<U> Cast<U>()
+        {
+            return this.hasValue ? new Maybe<U>((U)(object)this.value) : new Maybe<U>();
+        }
+
+        /// <summary>Converts the inner value of this instance if it exists and can safely be converted to the given type.</summary>
+        /// <typeparam name="U">The type to convert the inner value to.</typeparam>
+        /// <returns>
+        /// An empty maybe if this instance is empty, or if the inner value cannot be safely converted to type <typeparamref name="U"/>.
+        /// Otherwise, returns a maybe encapsulating the inner converted value.
+        /// </returns>
+        public Maybe<U> OfType<U>()
+        {
+            if (this.hasValue && this.value is U) return new Maybe<U>((U)(object)this.value);
+            else return new Maybe<U>();
+        }
+
+        /// <summary>Gets an enumerator for this instance. If this instance is empty then returned sequence is empty, otherwise the sequence contains the inner value.</summary>
+        /// <returns>An enumerator for the sequence represented by this instance.</returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (this.hasValue)
+            {
+                yield return this.value;
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        /// <summary>Override of Object.Equals.</summary>
+        /// <param name="obj">The object to compare against this instance.</param>
+        /// <returns>True if <paramref name="obj"/> is the same type as this instance and is equal according to <see cref="Maybe{T}.Equals(Maybe{T})"/>.</returns>
+        public override bool Equals(object obj)
+        {
+            return (obj is Maybe<T>) && this.Equals((Maybe<T>)obj);
+        }
+
+        /// <summary>Compares this instance for equality to another maybe value.</summary>
+        /// <param name="other">The maybe to compare with this instance.</param>
+        /// <returns>True if this and <paramref name="other"/> are both empty, or if they both have values and the values are equal. Otherwise false.</returns>
+        public bool Equals(Maybe<T> other)
+        {
+            if (other.hasValue != this.hasValue) return false;
+            else if (other.hasValue && this.hasValue) return EqualityComparer<T>.Default.Equals(this.value, other.value);
+            else return true;
+        }
+
+        /// <summary>Override of Object.GetHashCode.</summary>
+        /// <returns>A hash code for this instance. Empty values for different types are not guarenteed to return the same hash code.</returns>
+        public override int GetHashCode()
+        {
+            return this.hasValue ? this.value.GetHashCode() : typeof(T).GetHashCode();
+        }
+
+        /// <summary>Override of Object.ToString.</summary>
+        /// <returns>A description of the value if one exists, otherwise "None".</returns>
+        public override string ToString()
+        {
+            return this.hasValue ? string.Format("Some({0})", this.value) : "None";
+        }
+    }
+
+    /// <summary>Utility class for creating <see cref="Maybe{T}"/> values.</summary>
+    public static class Maybe
+    {
+        /// <summary>Creates an empty maybe of the given type.</summary>
+        /// <typeparam name="T">The type of maybe to create.</typeparam>
+        /// <returns>An empty maybe with the given value type.</returns>
+        public static Maybe<T> None<T>() { return new Maybe<T>(); }
+
+        /// <summary>Creates a maybe containing the given value.</summary>
+        /// <typeparam name="T">The inner value type.</typeparam>
+        /// <param name="value">The value of the maybe.</param>
+        /// <returns>A maybe containing <paramref name="value"/>.</returns>
+        public static Maybe<T> Some<T>(T value)
+        {
+            Contract.Requires(value != null);
+            return new Maybe<T>(value);
+        }
+    }
+
+    /// <summary>Extension methods for <see cref="Maybe{T}"/>.</summary>
+    public static class MaybeExtensions
+    {
+        /// <summary>Applicative instance for maybes.</summary>
+        /// <typeparam name="T">Argument type for the delegate encapsulated by <paramref name="mf"/>.</typeparam>
+        /// <typeparam name="U">Result type for the delegate encapsulated by <paramref name="mf"/>.</typeparam>
+        /// <param name="mf">Maybe instance with an inner delegate to apply.</param>
+        /// <param name="mv">A maybe instance with an inner argument to the encapsulated delegate.</param>
+        /// <returns>
+        /// If <paramref name="mf"/> and <paramref name="mv"/> both have values, then an maybe instance encapsulating the delegate to the value in <paramref name="mv"/>.
+        /// Otherwise an empty maybe.
+        /// </returns>
+        public static Maybe<U> Ap<T, U>(this Maybe<Func<T, U>> mf, Maybe<T> mv)
+        {
+            return mf.HasValue && mv.HasValue ? new Maybe<U>(mf.Value(mv.Value)) : new Maybe<U>();
+        }
+
+        /// <summary>Collapses a nested maybe value.</summary>
+        /// <typeparam name="T">The type of the value for the inner maybe.</typeparam>
+        /// <param name="mm">The nested maybe to collapse.</param>
+        /// <returns>The inner maybe instance for <paramref name="mm"/>.</returns>
+        public static Maybe<T> Join<T>(this Maybe<Maybe<T>> mm)
+        {
+            return mm.HasValue ? mm.Value : new Maybe<T>();
+        }
+
+        /// <summary>Converts a nullable struct to a maybe instance.</summary>
+        /// <typeparam name="T">The inner struct type.</typeparam>
+        /// <param name="n">The nullable value to convert.</param>
+        /// <returns>An empty maybe if <paramref name="n"/> is null, otherwise a maybe instance encapsulating the inner value.</returns>
+        public static Maybe<T> ToMaybe<T>(this T? n) where T : struct
+        {
+            return n.HasValue ? new Maybe<T>(n.Value) : new Maybe<T>();
+        }
+
+        /// <summary>Wraps a value in a <see cref="Maybe{T}"/>.</summary>
+        /// <typeparam name="T">The value to wrap.</typeparam>
+        /// <param name="value">The value to convert.</param>
+        /// <returns>An empty maybe if <paramref name="value"/> is null, otherwise a maybe instance encapsulating the inner value.</returns>
+        public static Maybe<T> ToMaybe<T>(this T value)
+        {
+            return value == null ? new Maybe<T>() : new Maybe<T>(value);
+        }
+
+        /// <summary>Converts the value of the given maybe to a nullable value type.</summary>
+        /// <typeparam name="T">The value type of the maybe.</typeparam>
+        /// <param name="maybe">The maybe.</param>
+        /// <returns>Null if <paramref name="maybe"/> is empty, otherwise the inner value.</returns>
+        public static T? ToNullable<T>(this Maybe<T> maybe) where T : struct
+        {
+            return maybe.HasValue ? maybe.Value : (T?)null;
+        }
+
+        /// <summary>Gets the value from the given maybe, or null if there is no value.</summary>
+        /// <typeparam name="T">The value type of the maybe.</typeparam>
+        /// <param name="maybe">The mabye.</param>
+        /// <returns>Null if <paramref name="maybe"/> is empty, otherwise the non-null reference value.</returns>
+        public static T ToClass<T>(this Maybe<T> maybe) where T : class
+        {
+            return maybe.GetOr((T)null);
+        }
+    }
+}
