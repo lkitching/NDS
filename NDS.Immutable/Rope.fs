94e6cf479732fd79b1cf3ac0cafb3d45374b3cc4
lkitching <lee.kitching@gmail.com>
2016-03-13 22:23:21 +0000

    Add immutable Rope implementation.
    Add module for Rope implementation with OO wrapper type.

---
+++ NDS.Immutable/Rope.fs
@@ -0,0 +1,142 @@
+﻿namespace NDS.Immutable
+module ModRope =
+    open System;
+    open System.Text;
+
+    type RopeNode = Nil | Leaf of string | Concat of int * RopeNode * RopeNode
+
+    ///<summary>The empty rope.</summary>
+    let empty = RopeNode.Nil
+
+    ///<summary>Creates a rope from the given string.</summary>
+    let ofString (s: String) =
+        match s with
+        | null -> Nil
+        | s when s.Length = 0 -> Nil
+        | s -> Leaf(s)
+
+    ///<summary>Gets the length of a rope</summary>
+    let length = function
+    | Nil -> 0
+    | Leaf(s) -> s.Length
+    | Concat(len, _, _) -> len
+
+    ///<summary>
+    ///Gets the character at the given index in a rope. Returns None if the index is out of range.
+    ///</summary>
+    let rec charAt i = function
+    | Nil -> None
+    | Leaf(s) -> Some(s.[i])
+    | Concat(len, l, r) ->
+        match i with
+        | _ when i < 0 -> None
+        | _ when i >= len -> None
+        | _ when i < (length l) -> charAt i l
+        | _ -> charAt (i - (length l)) r
+
+    ///<summary>Concats two ropes.</summary>
+    let concat r1 r2 =
+        match r1, r2 with
+        | Nil, _ -> r2
+        | _, Nil -> r1
+        | _, _ -> let l = (length r1) + (length r2) in Concat(l, r1, r2)
+
+    ///<summary>Splits a rope at the given index.</summary>
+    ///<param name="i">The index to split at.</param>
+    ///<param name="r">The rope to split.</param>
+    ///<returns>
+    ///A pair (left, right) where left contains a rope representing the string [0, i) and right contains
+    ///a rope representing [i, (length r)) of the source rope <paramref name="r"/>.
+    ///</returns>
+    let rec splitAt i r =
+        let len = (length r)
+        match i with
+        | 0 -> (Nil, r)
+        | _ when i < 0 || i > len -> raise (new ArgumentOutOfRangeException())
+        | _ when i = len -> (r, Nil)
+        | _ -> 
+            match r with
+            | Nil -> failwith "should never happen!"
+            | Leaf(s) -> let ls = s.Substring(0, i)
+                         let rs = s.Substring(i)
+                         (Leaf(ls), Leaf(rs))
+            | Concat(len, l, r) ->
+                let llen = length l
+                if i = llen then (l, r)
+                elif i < llen then
+                    let (ll, lr) = splitAt i l
+                    (ll, concat lr r)
+                else
+                    let (rl, rr) = splitAt (i - llen) r
+                    (concat l rl, rr)
+
+    ///<summary>Inserts a rope into another at the given index.</summary>
+    let insertAt i toInsert r =
+        let (left, right) = splitAt i r
+        concat (concat left toInsert) right
+
+    ///<summary>Removes a range [fromIndex, toIndex) from a rope.</summary>
+    ///<param name="fromIndex">Start index of the range to remove.</param>
+    ///<param name="toIndex">Exclusive end index of the range to remove.</param>
+    ///<param name="r">The rope to remove from.</param>
+    let removeRange fromIndex toIndex r =
+        let (left, middle) = splitAt fromIndex r
+        let (removed, right) = splitAt (toIndex - fromIndex) middle
+        concat left right
+
+    let rec private toStringAcc (sb: StringBuilder) r k =
+        match r with
+        | Nil -> k sb
+        | Leaf(s) ->
+            sb.Append(s) |> ignore
+            k sb
+        | Concat(_, l, r) -> toStringAcc sb l (fun sb' -> toStringAcc sb' r (fun sb'' -> k sb''))
+
+    ///<summary>Converts a rope to a string.</summary>
+    let toString r = toStringAcc (new StringBuilder()) r (fun sb -> sb.ToString())
+
+///<summary>An immutable string type which supports efficient concatenation and insertion into and removal from the middle.</summary>
+type public Rope private (root: ModRope.RopeNode) =
+    ///<summary>Gets the length of this rope.</summary>
+    member this.Length = ModRope.length root
+
+    ///<summary>Gets the character at the given index within this rope.</summary>
+    ///<param name="index">The index to retrieve</param>
+    ///<returns>The character at index <paramref name="index"/> in this rope.</returns>
+    ///<exception cref="IndexOutOfRange">If <paramref name="index"/> is not in the range [0, this.Length).</exception>
+    member this.Item
+        with get(index) =
+            match (ModRope.charAt index root) with
+            | None -> raise (new System.IndexOutOfRangeException())
+            | Some(c) -> c
+
+    ///<summary>Appends the given rope to this instance.</summary>
+    member this.Append(other: Rope) = new Rope(ModRope.concat root other.Root)
+
+    ///<summary>Prepends the given rope to the beginning of this rope.</summary>
+    member this.Prepend(other: Rope) = new Rope(ModRope.concat other.Root root)
+
+    ///<summary>Splits this rope at the given index.</summary>
+    ///<param name="index">The index to split at.</param>
+    ///<returns>A pair of ropes representing the ranges ([0, index), [index, this.Length)) of this instance.</returns>
+    ///<exception cref="ArgumentOutOfRangeException">If <paramref name="index"> is not in the range [0, this.Length].
+    member this.SplitAt(index) = let (lr, rr) = ModRope.splitAt index root in (new Rope(lr), new Rope(rr))
+
+    ///<summary>Inserts a rope into this instance at the given index.</summary>
+    ///<param name="index">The index to insert at</param>
+    ///<param name="toInsert">The rope to insert.</param>
+    ///<returns>A new rope representing [[0, index), toInsert, [index, this.Length)]</returns>
+    member this.InsertAt(index, toInsert: Rope) = new Rope(ModRope.insertAt index toInsert.Root root)
+    member this.RemoveRange(fromIndex, toIndex) = new Rope(ModRope.removeRange fromIndex toIndex root)
+
+    ///<summary>Gets the string representation of this rope.</summary>
+    override this.ToString() = ModRope.toString root
+    member private this.Root = root
+
+    ///<summary>The empty rope.</summary>
+    static member Empty = new Rope(ModRope.empty)
+
+    ///<summary>Creates a new rope from the given string.</summary>
+    static member FromString(s: string) = new Rope(ModRope.ofString s)
+
+
---
+++ NDS.Tests/Immutable/RopeTests.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+using NUnit.Framework;
+using NDS.Immutable;
+
+namespace NDS.Tests.Immutable
+{
+    [TestFixture]
+    public class RopeTests
+    {
+        [Test]
+        public void Empty_Should_Have_Length_Zero()
+        {
+            Assert.AreEqual(0, Rope.Empty.Length, "Unexpected length for empty rope");
+        }
+
+        [Test]
+        public void Should_Convert_Empty_To_Empty_String()
+        {
+            Assert.AreEqual(string.Empty, Rope.Empty.ToString(), "Unexpected string representation of empty rope");
+        }
+
+        [Test]
+        public void Should_Create_From_Null()
+        {
+            Assert.AreEqual(0, Rope.FromString(null).Length, "Rope from null string should be empty");
+        }
+
+        [Test]
+        public void Should_Create_From_String()
+        {
+            var s = "test string";
+            var rope = Rope.FromString(s);
+            Assert.AreEqual(s, rope.ToString());
+        }
+
+        [Test]
+        public void Should_Get_At_Index()
+        {
+            var s = "test string";
+            var index = new Random().Next(0, s.Length);
+            var r = Rope.FromString(s);
+
+            Assert.AreEqual(s[index], r[index], "Unexpected character at index {0}", index);
+        }
+
+        [Test]
+        public void Indexer_Should_Throw_If_Index_Negative()
+        {
+            var r = Rope.FromString("test");
+            Assert.Throws<IndexOutOfRangeException>(() => { char _ = r[-1]; });
+        }
+
+        [Test]
+        public void Indexer_Should_Throw_If_Index_Too_Large()
+        {
+            var r = Rope.FromString("test");
+            Assert.Throws<IndexOutOfRangeException>(() => { char _ = r[r.Length]; });
+        }
+
+        [Test]
+        public void Should_Append()
+        {
+            var s1 = "foo";
+            var s2 = "bar";
+            var appended = Rope.FromString(s1).Append(Rope.FromString(s2));
+
+            string expected = s1 + s2;
+            Assert.AreEqual(expected, appended.ToString(), "Unexpected appended value");
+        }
+
+        [Test]
+        public void Should_Prepend()
+        {
+            var s1 = "foo";
+            var s2 = "bar";
+            var prepened = Rope.FromString(s1).Prepend(Rope.FromString(s2));
+
+            string expected = s2 + s1;
+            Assert.AreEqual(expected, prepened.ToString(), "Unexpected prepended value");
+        }
+
+        [Test]
+        public void SplitAt_Zero_Should_Equal_Original()
+        {
+            var s = "input";
+            var split = Rope.FromString(s).SplitAt(0);
+            Assert.AreEqual(0, split.Item1.Length, "Left split result should be empty");
+            Assert.AreEqual(s, split.Item2.ToString(), "Right split result should equal original");
+            
+        }
+
+        [Test]
+        public void SplitAt_Length_Should_Equal_Original()
+        {
+            var s = "test string";
+            var split = Rope.FromString(s).SplitAt(s.Length);
+            Assert.AreEqual(s, split.Item1.ToString(), "Left split result should equal original");
+            Assert.AreEqual(0, split.Item2.Length, "Right split result should be empty");
+        }
+
+        [Test]
+        public void SplitAt_Should_Throw_If_Index_Negative()
+        {
+            var r = Rope.FromString("test");
+            Assert.Throws<ArgumentOutOfRangeException>(() => { var _ = r.SplitAt(-1); });
+        }
+
+        [Test]
+        public void Split_At_Should_Throw_If_Index_Greater_Than_Length()
+        {
+            var r = Rope.FromString("test string");
+            Assert.Throws<ArgumentOutOfRangeException>(() => { var _ = r.SplitAt(r.Length + 1); });
+        }
+
+        [Test]
+        public void Should_Insert()
+        {
+            var r = Rope.FromString("foobaz");
+            var inserted = r.InsertAt(3, Rope.FromString("bar"));
+            Assert.AreEqual("foobarbaz", inserted.ToString(), "Unexpected value after insert");
+        }
+
+        [Test]
+        public void Should_Remove_Range()
+        {
+            var r = Rope.FromString("bananarama");
+            var removed = r.RemoveRange(4, 8);
+            Assert.AreEqual("banama", removed.ToString(), "Unexpected value after removal");
+        }
+    }
+}
