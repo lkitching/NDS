4d5ec2d510ed0f8b0cca9f4458bf792df0bc19b4
lkitching <lee.kitching@gmail.com>
2016-09-28 23:09:23 +0100

    Add LexicographicComparer<T>.
    Add LexicographicComparer<T> which compares two sequences according
    to the order of their corresponding elements.

---
+++ NDS.Tests/LexicographicComparerTests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace NDS.Tests
+{
+    [TestFixture]
+    public class LexicographicComparerTests
+    {
+        [Test]
+        public void Empty_Sequences_Should_Be_Equal()
+        {
+            var comp = new LexicographicComparer<char>();
+            Assert.AreEqual(0, comp.Compare("", ""), "Empty sequences should be equal");
+        }
+
+        [Test]
+        public void Equal_Sequences_Should_Be_Equal()
+        {
+            var seq = TestGen.NRandomInts(50, 100).ToArray();
+            var comp = new LexicographicComparer<int>();
+
+            Assert.AreEqual(0, comp.Compare(seq, seq), "Sequences should be equal");
+        }
+
+        [Test]
+        public void Should_Be_Smaller()
+        {
+            var comp = new LexicographicComparer<char>();
+            Assert.Less(comp.Compare("aaa", "zzz"), 0);
+        }
+
+        [Test]
+        public void Should_Be_Larger()
+        {
+            var comp = new LexicographicComparer<char>();
+            Assert.Greater(comp.Compare("aabc", "aaab"), 0);
+        }
+
+        [Test]
+        public void Prefix_Sequence_Should_Be_Smaller()
+        {
+            var comp = new LexicographicComparer<int>();
+            var pre = TestGen.NRandomInts(50, 100).ToArray();
+            var rest = TestGen.NRandomInts(50, 100).ToArray();
+
+            Assert.Less(comp.Compare(pre, pre.Concat(rest)), 0, "Prefix sequence should be smaller");
+        }
+
+        [Test]
+        public void Suffix_Sequence_Should_Be_Larger()
+        {
+            var comp = new LexicographicComparer<int>();
+            var prefix = TestGen.NRandomInts(50, 100).ToArray();
+            var rest = TestGen.NRandomInts(50, 100).ToArray();
+
+            var longer = prefix.Concat(rest);
+            Assert.Greater(comp.Compare(longer, prefix), 0, "Longer sequence should be larger");
+        }
+
+        [Test]
+        public void Should_Be_Symmetric()
+        {
+            var comp = new LexicographicComparer<int>();
+            var x = TestGen.NRandomInts(5, 10).ToArray();
+            var y = TestGen.NRandomInts(5, 10).ToArray();
+
+            Assert.AreEqual(comp.Compare(x, y), -1 * comp.Compare(y, x), "Comparison should be symmetric");
+        }
+    }
+}
---
+++ NDS/LexicographicComparer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace NDS
+{
+    /// <summary>Compares two sequences lexicographically.</summary>
+    /// <typeparam name="T">The element type of the input sequences.</typeparam>
+    public class LexicographicComparer<T> : IComparer<IEnumerable<T>>
+    {
+        private readonly IComparer<T> comp;
+
+        /// <summary>Creates a new instance with the default comparer for <typeparamref name="T"/>.</summary>
+        public LexicographicComparer() : this(Comparer<T>.Default) { }
+
+        /// <summary>Creates a new instance with the given comparer for <typeparamref name="T"/>.</summary>
+        /// <param name="comp">Comparer for the items in the input sequences.</param>
+        public LexicographicComparer(IComparer<T> comp)
+        {
+            Contract.Requires(comp != null);
+            this.comp = comp;
+        }
+
+        /// <summary>
+        /// Compares two sequences lexicographically.
+        /// Compares each item in the two sequences elementwise. If the items differ according to the comparer
+        /// the result of the comparison is the result of the elementwise comparison. If the items are equal
+        /// the comparison continues. If one sequence is exhausted before the other it is the smaller sequence
+        /// according to this comparer. If the sequences are the same length and contain the same items 0 is
+        /// returned.
+        /// </summary>
+        /// <param name="x">The first sequence.</param>
+        /// <param name="y">The second sequence.</param>
+        /// <returns>
+        /// 0 if <paramref name="x"/> and <paramref name="y"/> are the same length and contain the same items
+        /// according to the item comparer. If the result of any comparison between items is non-zero that is
+        /// the value returned by this method.
+        /// </returns>
+        public int Compare(IEnumerable<T> x, IEnumerable<T> y)
+        {
+            using (var xEnum = x.GetEnumerator())
+            using (var yEnum = y.GetEnumerator())
+            {
+                while(xEnum.MoveNext())
+                {
+                    if(yEnum.MoveNext())
+                    {
+                        //compare x.Current and y.Current
+                        int c = this.comp.Compare(xEnum.Current, yEnum.Current);
+
+                        //if current element in x and y is equal continue iterating otherwise terminate with result
+                        //for current elements
+                        if (c != 0) return c;
+                    }
+                    else
+                    {
+                        //y is prefix of x so x is larger
+                        return 1;
+                    }
+                }
+
+                //if y has next element then x is a prefix of y and so is smaller
+                //if y also has no next element then the sequences are equal elementwise according to comp
+                return yEnum.MoveNext() ? -1 : 0;
+            }
+        }
+    }
+}
