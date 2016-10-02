b65224fcc717a84312ebee775bab9944b26c4fb6
lkitching <lee.kitching@gmail.com>
2016-10-02 22:38:58 +0100

    Add int range type.
    Add IntRange which represents an immutable integer range [start, end).

---
+++ NDS.Tests/IntRangeTests.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace NDS.Tests
+{
+    [TestFixture]
+    public class IntRangeTests
+    {
+        [Test]
+        public void ShouldBeEmpty()
+        {
+            var range = Empty();
+            Assert.IsTrue(range.IsEmpty, "Should be empty when start >= end");
+        }
+
+        [Test]
+        public void ShouldNotBeEmpty()
+        {
+            var r = new Random();
+            int start = r.Next(1000);
+            int count = r.Next(100, 10000);
+
+            Assert.IsFalse(new IntRange(start, start + count).IsEmpty, "Should not be empty when start < end");
+        }
+
+        [Test]
+        public void ShouldGetNonEmptyCount()
+        {
+            var r = new Random();
+            int start = r.Next(1000);
+            int count = r.Next(100, 10000);
+
+            Assert.AreEqual(new IntRange(start, start + count).Count, count);
+        }
+
+        [Test]
+        public void ShouldGetEmptyCount()
+        {
+            var range = Empty();
+            Assert.AreEqual(0, range.Count, "Empty range should have count = 0");
+        }
+
+        [Test]
+        public void ShouldContainItemsWithinRange()
+        {
+            var range = NonEmpty();
+            for(int i = range.Start; i < range.End; ++i)
+            {
+                Assert.IsTrue(range.Contains(i), "Should contain item within range");
+            }
+        }
+
+        [Test]
+        public void ShouldNotContainEnd()
+        {
+            var range = NonEmpty();
+            Assert.IsFalse(range.Contains(range.End), "Should not contain end element");
+        }
+
+        [Test]
+        public void ShouldNotContainItemBeforeStart()
+        {
+            var range = NonEmpty();
+            Assert.IsFalse(range.Contains(range.Start - 1));
+        }
+
+        [Test]
+        public void IndexerShouldThrowIfIndexNegative()
+        {
+            var range = NonEmpty();
+            Assert.Throws<ArgumentOutOfRangeException>(() => { int _ = range[-1]; });
+        }
+
+        [Test]
+        public void IndexerShouldThrowIfIndexTooLarge()
+        {
+            var range = NonEmpty();
+            Assert.Throws<ArgumentOutOfRangeException>(() => { int _ = range[range.Count]; });
+        }
+
+        [Test]
+        public void ShouldEnumerate()
+        {
+            var range = NonEmpty();
+            CollectionAssert.AreEqual(range, Enumerable.Range(range.Start, range.Count));
+        }
+
+        [Test]
+        public void ShouldDropSome()
+        {
+            var range = NonEmpty();
+            int toDrop = new Random().Next(1, range.Count);
+            var dropped = range.Drop(toDrop);
+
+            int expectedCount = range.Count - toDrop;
+
+            Assert.AreEqual(expectedCount, dropped.Count, "Unexepcted count for dropped range");
+            CollectionAssert.AreEqual(Enumerable.Range(range.End - expectedCount, expectedCount), dropped);
+        }
+
+        [Test]
+        public void ShouldDropAll()
+        {
+            var range = NonEmpty();
+            var dropped = range.Drop(range.Count);
+
+            Assert.IsTrue(dropped.IsEmpty, "Should drop all items");
+        }
+
+        [Test]
+        public void ShouldTake()
+        {
+            var range = NonEmpty();
+            int toTake = new Random().Next(1, range.Count);
+            var taken = range.Take(toTake);
+
+            CollectionAssert.AreEqual(Enumerable.Range(range.Start, toTake), taken);
+        }
+
+        private static IntRange Empty()
+        {
+            var r = new Random();
+            int start = r.Next(100, 1000);
+            int diff = r.Next(10);
+            return new IntRange(start, start - diff);
+        }
+
+        private static IntRange NonEmpty()
+        {
+            var r = new Random();
+            int start = r.Next(1000);
+            int count = r.Next(100, 10000);
+            return new IntRange(start, start + count);
+        }
+    }
+}
---
+++ NDS/IntRange.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace NDS
+{
+    /// <summary>Represents a consecutive range [start, end).</summary>
+    public struct IntRange : IReadOnlyList<int>, IEquatable<IntRange>
+    {
+        /// <summary>Creates a range [0, end).</summary>
+        /// <param name="end">Exclusive end element of the range.</param>
+        public IntRange(int end) : this(0, end) { }
+
+        /// <summary>Creates a range [start, end).</summary>
+        /// <param name="start">Inclusive start element of the range.</param>
+        /// <param name="end">Exclusive end element of the range.</param>
+        public IntRange(int start, int end): this()
+        {
+            this.Start = start;
+            this.End = end;
+        }
+
+        /// <summary>Gets the start element of this range.</summary>
+        public int Start { get; private set; }
+
+        /// <summary>Gets the exclusive end element of this range.</summary>
+        public int End { get; private set; }
+
+        /// <summary>Whether this range is empty.</summary>
+        public bool IsEmpty
+        {
+            get { return this.Start >= this.End; }
+        }
+
+        /// <summary>Returns whether the given value is contained within this range.</summary>
+        /// <param name="i">The item to find.</param>
+        /// <returns>Whether <paramref name="i"/> exists within this range.</returns>
+        public bool Contains(int i)
+        {
+            return i >= this.Start && i < this.End;
+        }
+
+        /// <summary>Gets the number of elements in this range.</summary>
+        public int Count
+        {
+            get
+            {
+                if (this.IsEmpty) return 0;
+                else return this.End - this.Start;
+            }
+        }
+
+        /// <summary>Gets the item at the given index in this range.</summary>
+        /// <param name="idx">The index to fetch./param>
+        /// <returns>The item at the given index in this range.</returns>
+        public int this[int idx]
+        {
+            get
+            {
+                if(idx < 0 || idx >= this.Count)
+                {
+                    string msg = string.Format("Index must be in range [0, {0})", this.Count);
+                    throw new ArgumentOutOfRangeException("idx", idx, msg);
+                }
+                Contract.EndContractBlock();
+
+                return this.Start + idx;
+            }
+        }
+
+        /// <summary>Returns a new range [start + count, end).</summary>
+        /// <param name="count">The number of items to drop from the start of this range.</param>
+        /// <returns>A new range [start + count, end)</returns>
+        public IntRange Drop(int count)
+        {
+            Contract.Requires(count >= 0);
+            return new IntRange(this.Start + count, this.End);
+        }
+
+        /// <summary>Returns a new range [start, end - count).</summary>
+        /// <param name="count">The number of items to keep from the start of this range.</param>
+        /// <returns>A new range [start, end - count).</returns>
+        public IntRange Take(int count)
+        {
+            Contract.Requires(count >= 0);
+            return (count >= this.Count) ? this : new IntRange(this.Start, this.Start + count);
+        }
+
+        /// <summary>Gets an enumerator for the items in this range.</summary>
+        /// <returns>An enumerator for the items in this range.</returns>
+        public IEnumerator<int> GetEnumerator()
+        {
+            for(int i = this.Start; i < this.End; ++i)
+            {
+                yield return i;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        /// <summary>Whether this range is equal to another. The two ranges are equal if their start and end elements are equal.</summary>
+        /// <param name="other">The range to compare to this.</param>
+        /// <returns>Whether this range is equal to <paramref name="other"/>.</returns>
+        public bool Equals(IntRange other)
+        {
+            return this.Start == other.Start && this.End == other.End;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is IntRange && this.Equals((IntRange)obj);
+        }
+
+        /// <summary>Gets a hashcode for this range.</summary>
+        /// <returns>A hashcode for this range.</returns>
+        public override int GetHashCode()
+        {
+            return this.Start + 17 * this.End;
+        }
+    }
+}
