6cc9778181a8d6371d91e150d58dd0fcdbac628e
lkitching <lee.kitching@gmail.com>
2016-10-22 12:11:24 +0100

    Move MinimumBy method into Seq class.

--- NDS/Seq.cs
+++ NDS/Seq.cs
@@ -60,5 +60,27 @@ namespace NDS
                 else return true;
             }
         }
+
+        public static Maybe<T> MinimumBy<T, TKey>(this IEnumerable<T> seq, Func<T, TKey> keyFunc, IComparer<TKey> keyComparer)
+        {
+            var min = Maybe.None<KeyValuePair<T, TKey>>();
+            foreach (T item in seq)
+            {
+                if (min.HasValue)
+                {
+                    TKey currentKey = keyFunc(item);
+                    if (keyComparer.CompareResult(currentKey, min.Value.Value) == ComparisonResult.Less)
+                    {
+                        min = Maybe.Some(new KeyValuePair<T, TKey>(item, currentKey));
+                    }
+                }
+                else
+                {
+                    min = Maybe.Some(new KeyValuePair<T, TKey>(item, keyFunc(item)));
+                }
+            }
+
+            return min.Select(kvp => kvp.Key);
+        }
     }
 }
