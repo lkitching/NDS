13cc629c2ca0a9baec1edafa96d5434c4d6de080
lkitching <lee.kitching@gmail.com>
2017-03-04 21:36:56 +0000

    Add immutable queue.
    Add implementation of immutable queue along with tests in the new
    NDS.Immutable.Tests assembly.
    
    Update NUnit to version 3.

---
+++ NDS.Immutable.Tests/QueueTests.fs
@@ -0,0 +1,67 @@
+﻿namespace NDS.Immutable.Tests
+
+open NDS.Immutable.ModQueue
+open NUnit.Framework
+open FsCheck
+open FsCheck.Gen
+
+[<TestFixture>]
+module QueueTests =
+    let genQueue<'a> () =
+        let listGen = Arb.Default.FsList<'a>().Generator
+        let gen' size =
+            match size with
+            | 0 -> gen { return empty }
+            | n -> gen {
+                 let! front = listGen
+                 let! back = listGen
+                 let queue = 
+                    match front, back with
+                    | ([], []) -> empty
+                    | (f::fs), _ -> { front = front; back = back }
+                    | [], (b::bs) -> { front = [b]; back = bs }
+                return queue
+            }
+        Gen.sized gen'
+
+    let enqueueAll l = List.fold (fun q e -> enqueue e q) empty l
+    let rec dequeueN n q =
+        if n = 0
+            then q
+            else dequeueN (n - 1) (snd (tryDequeue q))
+
+    let dequeueAll q = 
+        let dequeueState q =
+            let (eo, q') = tryDequeue q
+            eo |> Option.map (fun e -> (e, q'))
+        Seq.unfold dequeueState q
+
+    [<Test>]
+    let ``ofSeq >> toSeq = id`` () =
+        let p (l: int list) = List.ofSeq (toSeq (ofSeq l)) = l
+        Check.Quick p
+
+    [<Test>]
+    let ``should enqueue in order`` () =
+        let p (l: int list) = List.ofSeq (toSeq (enqueueAll l)) = l
+        Check.Quick p
+
+    [<Test>]
+    let ``should dequeue in order`` () =
+        let p (l: int list) = List.ofSeq (dequeueAll (enqueueAll l)) = l
+        Check.Quick p
+
+    [<Test>]
+    let ``dequeue should maintain items in front list`` () =
+        let deqGen = gen {
+            let! q = ((genQueue<int>()) |> Gen.filter (fun q -> length q > 1))
+            let! toDeq = Gen.choose (1, (length q - 1))
+            return (q, toDeq)
+        }
+        let p (q, n) =
+            let { front = f } = dequeueN n q
+            not (List.isEmpty f)
+
+        let prop = Prop.forAll (Arb.fromGen deqGen) p
+        Check.Quick prop
+
---
+++ NDS.Immutable/Queue.fs
@@ -0,0 +1,47 @@
+﻿namespace NDS.Immutable
+module ModQueue =
+    type Queue<'T> = { front : 'T list; back : 'T list }
+    let empty<'T> : Queue<'T> =  { front = []; back = [] }
+    let isEmpty ({ front = front; back = back }) = List.isEmpty front && List.isEmpty back
+    let enqueue e ({ front = front; back = back } as q) =
+        match front with
+        | [] -> 
+            match back with
+            | [] -> { front = [e]; back = []}
+            | rev -> { front = (List.rev back); back = [e] }
+        | _ -> { q with back = e :: back }
+
+    let tryDequeue ({ front = front; back = back } as q) =
+        match front with
+        | (f::s::es) -> (Some(f), { q with front = (s :: es)})
+        | [e] ->
+            match back with
+            | [] -> (Some(e), empty)
+            | rev -> (Some(e), { front = List.rev rev; back = [] })
+        | [] ->
+            match back with
+            | [] -> (None, q)
+            | rev ->
+                //items in back list are stored in reverse order
+                let (e::es) = List.rev rev
+                (Some(e), { front = es; back = []})
+
+    let peek { front = front } =
+        match front with
+        | [] -> None
+        | e::_ -> Some(e)
+
+    let length ({ front = front; back = back }) =
+        List.length front + List.length back
+
+    let ofSeq s = { front = List.ofSeq s; back = [] }
+    let toSeq ({ front = front; back = back}) =
+        seq {
+            yield! front
+            yield! (List.rev back)
+        }
+
+type public Queue<'T> private (q : ModQueue.Queue<'T>) =
+    member this.Enqueue(item: 'T) = new Queue<'T>(ModQueue.enqueue item q)
+    static member Empty = new Queue<'T>(ModQueue.empty)
+    static member OfSeq(s : 'T seq) = new Queue<'T>(ModQueue.ofSeq s)
