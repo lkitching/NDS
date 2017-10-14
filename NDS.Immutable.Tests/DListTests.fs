492f903d241fbeeb7aef750a087889528d6ac376
lkitching <lee.kitching@gmail.com>
2017-10-14 18:44:03 +0100

    Add DList implementation.

---
+++ NDS.Immutable.Tests/DListTests.fs
@@ -0,0 +1,89 @@
+﻿namespace NDS.Immutable.Tests
+
+open NUnit.Framework
+open FsCheck
+
+[<TestFixture>]
+module DListTests =
+    open NDS.Immutable.ModDList
+
+    [<Test>]
+    let ``empty is empty`` () =
+        Assert.AreEqual([], toList empty)
+
+    [<Test>]
+    let ``toList ofList is id`` () =
+        let p (xs : int list) = xs = toList (ofList xs)
+        Check.QuickThrowOnFailure p
+
+    [<Test>]
+    let ``append`` () =
+        let p (l1 : int list, l2 : int list) = toList (append (ofList l1) (ofList l2)) = l1 @ l2
+        Check.QuickThrowOnFailure p
+
+    [<Test>]
+    let ``singleton`` () =
+        let p (i : int) = toList (singleton i) =  [i]
+        Check.QuickThrowOnFailure p
+
+    [<Test>]
+    let ``cons`` () =
+        let p (i : int, xs : int list) = toList (cons i (ofList xs)) = i :: xs
+        Check.QuickThrowOnFailure p
+
+    [<Test>]
+    let ``snoc`` () =
+        let p (i : int, xs : int list) = toList (snoc i (ofList xs)) = xs @ [i]
+        Check.QuickThrowOnFailure p
+
+    [<Test>]
+    let ``head non empty`` () =
+        let p (i : int, xs : int list) = head (ofList (i::xs)) = i
+        Check.QuickThrowOnFailure p
+
+    [<Test>]
+    let ``tail non empty`` () =
+        let p (i : int, xs : int list) = toList (tail (ofList (i::xs))) = xs
+        Check.QuickThrowOnFailure p
+
+    [<Test>]
+    let ``concat`` () =
+       let p (xss : int list list) = toList (concat (List.map ofList xss)) = List.concat xss
+       Check.QuickThrowOnFailure p
+
+    [<Test>]
+    let ``replicate`` () =
+        let p (n : NonNegativeInt, s : string) = toList (replicate n.Get s) = List.replicate n.Get s
+        Check.QuickThrowOnFailure p
+
+    [<Test>]
+    let ``map`` () =
+        let f (s : string) =
+            match s with
+            | null -> 0
+            | _ -> s.Length
+            
+        let p (xs : string list) = toList (NDS.Immutable.ModDList.map f (ofList xs)) = List.map f xs
+        Check.QuickThrowOnFailure p
+
+    [<Test>]
+    let ``fold`` () =
+        let p (xs : int list) = fold (+) 0 (ofList xs) = List.fold (+) 0 xs
+        Check.QuickThrowOnFailure p
+
+    let makeUnfolder (l : int list) =
+        let mutable lr = l
+        fun (i : int) ->
+            match lr with
+            | [] -> None
+            | s :: ss ->
+                 lr <- ss
+                 Some((i, i + s))
+                
+    [<Test>]
+    let ``unfold`` () =        
+        let p (i : int) (steps : int list) =
+            let f1 = makeUnfolder steps
+            let f2 = makeUnfolder steps
+            Seq.unfold f1 i |> List.ofSeq = toList (unfold f2 i)
+        Check.QuickThrowOnFailure p
---
+++ NDS.Immutable/DList.fs
@@ -0,0 +1,82 @@
+﻿namespace NDS.Immutable
+
+module ModDList =
+    /// <summary>
+    /// Represents a list as a function which constructs a list by appending it to the front
+    /// of the argument list.
+    /// </summary>
+    type DList<'T> = DL of ('T list -> 'T list)
+
+    /// <summary>Construct a DList from the given list.</summary>
+    let ofList l = DL (fun l' -> List.append l l')
+
+    /// <summary>Materialises a DList to a list.
+    let toList (DL f) = f []
+
+    /// <summary>Emtpy DList.</summary>
+    let empty = DL id
+
+    /// <summary>Appends two DLists.
+    let append (DL f) (DL g) = DL (fun l' -> f (g l'))
+
+    /// <summary>Constructs a DList containing the given value.
+    let singleton x = DL (fun l' -> x :: l')
+
+    /// <summary>Appends the given item to the front of a DList.
+    let cons x (DL f) = DL (fun l' -> x :: (f l'))
+
+    /// <summary>Appends the given item to the end of a DList.
+    let snoc x (DL f) = DL (fun l' -> f (x :: l'))
+    let head dl =
+        match toList dl with
+        | [] -> failwith "Empty difference list"
+        | x::_ -> x
+    let tail dl =
+        match toList dl with
+        | [] -> failwith "Empty difference list"
+        | _::xs -> ofList xs
+
+    /// <summary>Concatenates a sequence of DLists into a single DList.
+    let concat dls = dls |> Seq.fold append empty
+
+    /// <summary>Creates a DList of <paramref name="e"/> replicated <paramref name="n"> times.
+    /// <param name="n">The number of replications.</param>
+    /// <param name="e">The value to replicate.</param>
+    let replicate n e =
+        let rec aux l n =
+            if n <= 0 then l else aux (e :: l) (n-1)
+        DL (fun l -> aux l n)
+
+    /// <summary>Transforms the elements of a DList by a transform function.</summary>
+    let map f dl : DList<'b> = 
+        DL (fun l' -> List.map f (toList dl))
+    let fold f init dl = List.fold f init (toList dl)
+
+    /// <summary>
+    /// Generates a DList from an initial value and a function which calculates the
+    /// next value and state if one exists. The generation is terminated by returning
+    /// None.
+    /// <param name="f">Function to generate next (value, state).</param>
+    /// <param name="init">Initial state.</param>
+    let rec unfold f init =
+        match f init with
+        | None -> empty
+        | Some(e, state) -> cons e (unfold f state)
+
+type public DList<'T> private (dl : ModDList.DList<'T>) =
+    member private this.DList = dl
+    member this.ToFSList = ModDList.toList dl   
+    member this.Append (other : DList<'T>) = new DList<_>(ModDList.append dl other.DList)
+    member this.Cons(x : 'T) = new DList<_>(ModDList.cons x dl)
+    member this.Snoc(x : 'T) = new DList<_>(ModDList.snoc x dl)
+    member this.Head() = ModDList.head dl
+    member this.Tail() = new DList<_>(ModDList.tail dl)
+    member this.Map(f) = new DList<_>(ModDList.map f dl)
+    member this.Fold(f, init) = ModDList.fold f init dl
+    
+    static member Empty with get() = new DList<_>(ModDList.empty)
+    static member Concat(dls : DList<'T> seq) = new DList<_>(dls |> Seq.map(fun dl -> dl.DList) |> ModDList.concat)
+    static member Unfold(f, init) = new DList<_>(ModDList.unfold f init)
+    static member Replicate(n, e) = new DList<_>(ModDList.replicate n e)
+
+
